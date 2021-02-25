ebfb51a39ac4f9e75c00b49247bf07d878c99020
Russell Davis <551404+russelldavis@users.noreply.github.com>
2021-02-24 18:33:02 -0800

    Clean up

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
 chrome.browserAction.onClicked.addListener(function() {
   chrome.tabs.executeScript({
     // From https://www.diigo.com/tools/post_to_diigo
-    code: "(function(){var url = location.href;var title = document.title || url;var desc = (window.getSelection ? window.getSelection().toString() : document.getSelection ? document.getSelection()  : document.selection.createRange().text);ww=window.open('https://www.diigo.com/post?url=' + encodeURIComponent(url) + '&title=' + encodeURIComponent(title) + '&desc=' + encodeURIComponent(desc)+'&client=simplelet#main','_blank','menubar=no,height=580,width=608,toolbar=no,scrollbars=no,status=no'); })();"
+    code: `(function(){
+      var url = location.href;
+      var title = document.title || url;
+      var desc = (window.getSelection ? window.getSelection().toString() : document.getSelection ? document.getSelection()  : document.selection.createRange().text);
+      ww=window.open(
+          'https://www.diigo.com/post?url=' + encodeURIComponent(url) +
+              '&title=' + encodeURIComponent(title.replace(/"/g, '&quot;')) +
+              '&desc=' + encodeURIComponent(desc.replace(/"/g, '&quot;')) +
+              '&client=simplelet#main',
+          '_blank',
+          'menubar=no,height=580,width=608,toolbar=no,scrollbars=no,status=no'
+      );
+    })();`
   });
 });
